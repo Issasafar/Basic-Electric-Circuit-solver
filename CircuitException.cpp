23fd9c645c5025e5883ece2802f746716510f453
issasafar <issasafar234@gmail.com>
2024-05-10 13:48:24 +0300

    done

---
+++ CircuitException.cpp
@@ -0,0 +1,5 @@
+//
+// Created by issa on 10/05/24.
+//
+
+#include "CircuitException.h"
---
+++ CircuitException.h
@@ -0,0 +1,14 @@
+//
+// Created by issa on 10/05/24.
+//
+
+#ifndef DEMOPROJECT_CIRCUITEXCEPTION_H
+#define DEMOPROJECT_CIRCUITEXCEPTION_H
+
+
+class CircuitException {
+
+};
+
+
+#endif //DEMOPROJECT_CIRCUITEXCEPTION_H
