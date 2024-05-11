824df733537a6c719b33733e49f2105bdfa11f52
issasafar <issasafar234@gmail.com>
2024-05-11 21:25:15 +0300

    implemented basic solve algorithim

---
+++ Circuit.cpp
@@ -0,0 +1,5 @@
+//
+// Created by issa on 11/05/24.
+//
+
+#include "Circuit.h"
---
+++ Circuit.h
@@ -0,0 +1,14 @@
+//
+// Created by issa on 11/05/24.
+//
+
+#ifndef DEMOPROJECT_CIRCUIT_H
+#define DEMOPROJECT_CIRCUIT_H
+
+
+class Circuit {
+
+};
+
+
+#endif //DEMOPROJECT_CIRCUIT_H
